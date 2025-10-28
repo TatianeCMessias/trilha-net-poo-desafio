56b01f095d1954a39a160308b44b564242f958d5
Tatiane Messias <tatianem600@gmail.com>
2025-10-28 14:06:50 -0300

    Subindo Sistema e Abistraindo um celular com poo em C#

--- Program.cs
+++ Program.cs
@@ -10,15 +10,17 @@ namespace DesaioPOO
             Smartphone nokia = new Nokia("81981040481", "nokia", "131314", 64);
             Smartphone iphone = new Iphone("81983557430", "iphone 15", "151515", 300);
             
+            Console.WriteLine(" ");
             Console.WriteLine("========== Smartphone Iphone ==========");
             iphone.InstalarAplicativo("WhatsApp");
             nokia.Ligar();
-
-            Console.WriteLine("=======================================");
+            
+            Console.WriteLine(" ");
 
             Console.WriteLine("========== Smartphone Nokia ==========");
             nokia.InstalarAplicativo("WhatsApp");
-            iphone.ReceberLigacao();    
+            iphone.ReceberLigacao();  
+            Console.WriteLine(" ");  
         }
     }
 }
